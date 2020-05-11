f6308bb0858602f9c81c79784e28af6a673b3512
panhuaqing <panhuaqing@bluetop.com.cn>
2020-05-11 17:24:57 +0800

    修改请求代理

--- vue.config.js
+++ vue.config.js
@@ -54,7 +54,6 @@ module.exports = {
     // }, // string | Object 111 举 /192.168.109.232 
     proxy: { //代理地址 可配置多个
       '/api': {
-       // target: 'http://192.168.107.72:8080',
         target: 'http://192.168.109.100:8081',
         ws: false,
         changeOrigin: true,
